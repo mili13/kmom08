8e92476798fc4e8f2d1894e0ca307df053dcf7e8
Michelle Lindholm <michelle.lindholm.gmail.com>
2013-05-15 12:57:13 +0200

    ändringar av länkar

--- src/CCIndex/index.tpl.php
+++ src/CCIndex/index.tpl.php
@@ -4,9 +4,9 @@
 <h2>Download</h2>
 <p>You can download Winnie from github.</p>
 <blockquote>
-<code>git clone git://github.com/mili13/kmom07.git</code>
+<code>git clone https://github.com/mili13/kmom08.git</code>
 </blockquote>
-<p>You can review its source directly on github: <a href='https://github.com/mili13/kmom07'>https://github.com/mili13/kmom07</a></p>
+<p>You can review its source directly on github: <a href='https://github.com/mili13/kmom08'>https://github.com/mili13/kmom07</a></p>
 
 <h2>Installation</h2>
 <p>First you have to make the data-directory writable. This is the place where Winnie needs
